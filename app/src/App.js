f934ee3d13df0d16aed3e160e7b7a83315e37ee8
Inioluwa Olaleye <olaleyeini7@gmail.com>
2023-04-01 17:39:31 -0600

    rename App to be in .js extension rather than .jsx

---
+++ app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+function App() {
+  return (
+    <div>
+      <LandingPage />
+    </div>
+  );
+}
+
+export default App;
