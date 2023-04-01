7823196ed292d9e2cf19bf91b234238e8c214cfb
Inioluwa Olaleye <olaleyeini7@gmail.com>
2023-04-01 17:43:37 -0600

    add routes to landing, register and sign in pages

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
+import RegisterPage from './pages/RegisterPage';
+import SignInPage from './pages/SignInPage';
 function App() {
   return (
-    <div>
-      <LandingPage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/Register" element={<RegisterPage />} />
+        <Route path="/Signin" element={<SignInPage />} />
+      </Routes>
+    </Router>
   );
 }
 
