c3ec1adec2e9d9a84504716c25bb639fdf6ad9c5
Inioluwa Olaleye <olaleyeini7@gmail.com>
2023-04-01 15:03:58 -0600

    create landing page component and page

---
+++ app/src/components/LandingComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function LandingComponent() {
+  return (
+    <div>
+      <h1>Deezcord is a real time chat tool</h1>
+      <h2>It is created by Inioluwa Olaleye for his 353 project</h2>
+      <h2>Create an account and begin chatting</h2>
+    </div>
+  );
+}
+
+export default LandingComponent;
