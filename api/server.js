538d56083d5198d7de3e4b42ba9dd8374a97d82f
Inioluwa Olaleye <olaleyeini7@gmail.com>
2023-04-01 15:48:57 -0600

    make the users, posts and replies table

--- api/server.js
+++ api/server.js
@@ -24,6 +24,52 @@ var connection = mysql.createConnection({
 
 connection.connect;
 
+connection.query(
+  `CREATE DATABASE IF NOT EXISTS deezdb`,
+  function (error, result) {
+    if (error) console.log(error);
+  }
+);
+connection.query(`USE deezdb`, function (error, results) {
+  if (error) console.log(error);
+});
+
+connection.query(
+  `CREATE TABLE IF NOT EXISTS users
+( id int unsigned NOT NULL auto_increment, username varchar(20)
+NOT NULL,
+password varchar(30) NOT NULL,
+PRIMARY KEY (id)
+)`,
+  function (error, result) {}
+);
+console.log('made users table');
+connection.query(
+  `CREATE TABLE IF NOT EXISTS posts
+( id int unsigned NOT NULL auto_increment,
+  post id int NOT NULL,
+data varchar(400) NOT NULL,
+userid int,
+upvotes int,
+downvotes int,
+PRIMARY KEY (id)
+)`,
+  function (error, result) {}
+);
+console.log('made posts table');
+connection.query(
+  `CREATE TABLE IF NOT EXISTS replies
+( id int unsigned NOT NULL auto_increment, topic varchar(30)
+NOT NULL,
+data varchar(400) NOT NULL,
+userid int,
+upvotes int,
+downvotes int,
+PRIMARY KEY (id)
+)`,
+  function (error, result) {}
+);
+console.log('made replies table');
 app.listen(PORT, () => {
   console.log(`Server running on port http://localhost:${PORT}`);
 });
