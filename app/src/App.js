36515314f9ab1eb17bf41cdf11a745a00c179144
Inioluwa Olaleye <olaleyeini7@gmail.com>
2023-04-06 09:45:22 -0600

    wrap routes in authprovider

--- app/src/App.js
+++ app/src/App.js
@@ -4,15 +4,18 @@ import LandingPage from './pages/LandingPage';
 import RegisterPage from './pages/RegisterPage';
 import SignInPage from './pages/SignInPage';
 import ChatPage from './pages/ChatPage';
+import { AuthProvider } from './AuthContext';
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/Register" element={<RegisterPage />} />
-        <Route path="/Signin" element={<SignInPage />} />
-        <Route path="/Chat" element={<ChatPage />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/Register" element={<RegisterPage />} />
+          <Route path="/Signin" element={<SignInPage />} />
+          <Route path="/Chat" element={<ChatPage />} />
+        </Routes>
+      </AuthProvider>
     </Router>
   );
 }
