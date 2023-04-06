11f5bd1d7965f6d35888a9206f266d7a67dda5d2
Inioluwa Olaleye <olaleyeini7@gmail.com>
2023-04-06 11:34:51 -0600

    display the replies to messages

--- app/src/components/MessageHistory.jsx
+++ app/src/components/MessageHistory.jsx
@@ -4,8 +4,14 @@ import { AuthContext } from '../AuthContext';
 
 function MessageHistory() {
   const { currentChannel } = useContext(AuthContext);
-
+  // const { messageId, setMessageId } = useState();
+  var messageId;
   const [messages, setMessages] = useState([]);
+  const [replies, setReplies] = useState([]);
+
+  function setTheMessageId(mId) {
+    messageId = mId;
+  }
   function postMessage() {
     const postInfo = {
       username: '',
@@ -18,8 +24,18 @@ function MessageHistory() {
 
     window.location.reload();
   }
-  function makeAReply(messageId){
+  function makeAReply() {
+    alert(messageId);
+    const replyInfo = {
+      username: '',
+      data: document.getElementById('data').value,
+      postId: messageId,
+      channelname: currentChannel,
+    };
 
+    axios
+      .post('http://localhost:81/postReply', replyInfo)
+      .then((response) => {});
   }
   function getAllMessages() {
     // alert(currentChannel);
@@ -29,11 +45,27 @@ function MessageHistory() {
     axios
       .get('http://localhost:81/getMessages', channelInfo)
       .then((response) => {
-        const theMessages = response.data.map((messages) => [messages.data, messages.id]);
+        const theMessages = response.data.map((messages) => [
+          messages.data,
+          messages.id,
+          messages.username,
+        ]);
         setMessages(theMessages);
       });
+    getAllReplies();
   }
 
+  function getAllReplies() {
+    axios.get('http://localhost:81/getReplies').then((response) => {
+      const theReplies = response.data.map((replies) => [
+        replies.data,
+        replies.postid,
+        replies.username,
+        replies.id,
+      ]);
+      setReplies(theReplies);
+    });
+  }
   useEffect(() => {
     getAllMessages();
   }, []);
@@ -41,8 +73,17 @@ function MessageHistory() {
     <div>
       {messages.map((item) => (
         <div key={item}>
-          <p>{item[0]}</p>
-          <button>Reply</button>
+          <p>
+            <b>{item[0]}</b>
+          </p>
+          {replies
+            .filter((replyItem) => replyItem[1] === item[1])
+            .map((replyItem) => (
+              <div key={replyItem}>
+                <p> {replyItem[0]}</p>
+              </div>
+            ))}
+          <button onClick={() => setTheMessageId(item[1])}>Reply</button>
         </div>
       ))}
       <br />
@@ -52,8 +93,15 @@ function MessageHistory() {
       <br />
       <br />
 
-      <button id="submitButton" onClick={postMessage}>
+      <button
+        id="submitButton"
+        className="theSubmitButton"
+        onClick={postMessage}
+      >
         Submit
+      </button>
+      <button id="replyButton" className="theReplyButton" onClick={makeAReply}>
+        Reply
       </button>
       <br></br>
     </div>
