8b2313643bb4927775479b57c5cfb00b9c1dc306
Inioluwa Olaleye <olaleyeini7@gmail.com>
2023-05-03 11:35:15 -0600

    add search page link to chat page

--- app/src/pages/ChatPage.jsx
+++ app/src/pages/ChatPage.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import SideBar from '../components/SideBar';
 import MessageHistory from '../components/MessageHistory';
+import { useNavigate } from 'react-router-dom';
 import '../stylings/chat.css';
 function ChatPage() {
+  const navigate = useNavigate();
+  function navigateToSearch() {
+    navigate('/Search');
+  }
   return (
     <div className="chat-page">
       <div className="side-bar">
-        <h1>CHAT</h1>
+        <h1>DEEZCORD</h1>
         <SideBar />
       </div>
       <div className="message-history">
         <MessageHistory />
+      </div>
+      <div>
+        <button className="" onClick={navigateToSearch}>
+          Search
+        </button>
       </div>
     </div>
   );
